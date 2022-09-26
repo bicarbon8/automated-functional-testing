626d6d3002c228fcc3e20873d33b4a9fe7759dfc
Jason Holt Smith <bicarbon8@gmail.com>
2022-09-26 10:09:35 +0000

    adding functional test execution to github actions

--- packages/aft-examples/test/ui/browser-tests/browsers.spec.ts
+++ packages/aft-examples/test/ui/browser-tests/browsers.spec.ts
@@ -1,10 +1,12 @@
-import { containing, retry, Verifier, verify, wait } from "aft-core";
+import { containing, retry, Verifier, verify } from "aft-core";
+import { AftLog, AftTest } from "aft-mocha-reporter";
 import { verifyWithBrowser, BrowserVerifier, BrowserFacetOptions } from "aft-ui-browsers";
 import { expect } from "chai";
 import { HerokuLoginPage } from "./page-objects/heroku-login-page";
 
 describe('Functional Browser Tests using AFT-UI-BROWSERS', () => {
-    it('can access websites using AFT and Page Widgets and Facets', async () => {
+    it('can access websites using AFT and Page Widgets and Facets', async function() {
+        const aft = new AftLog(this);
         await verifyWithBrowser(async (tw: BrowserVerifier) => {
             let loginPage: HerokuLoginPage = await tw.session.getFacet<HerokuLoginPage, BrowserFacetOptions>(HerokuLoginPage);
             
@@ -22,12 +24,17 @@ describe('Functional Browser Tests using AFT-UI-BROWSERS', () => {
             
             await tw.logMgr.step('get message...');
             return await loginPage.getMessage();
-        }).withDescription('can access websites using AFT and Page Widgets and Facets')
+        }).withLogManager(aft.logMgr)
         .and.withTestIds('C3456').and.withTestIds('C2345').and.withTestIds('C1234')
         .returns(containing("You logged into a secure area!"));
     });
 
-    it('can recover from StaleElementExceptions automatically', async () => {
+    it('can recover from StaleElementExceptions automatically', async function() {
+        const aft = new AftTest(this);
+        const shouldRun = await aft.shouldRun();
+        if (!shouldRun) {
+            this.skip();
+        }
         await verifyWithBrowser(async (tw: BrowserVerifier) => {
             let loginPage: HerokuLoginPage = await tw.session.getFacet<HerokuLoginPage, BrowserFacetOptions>(HerokuLoginPage, {maxWaitMs: 5000});
             
@@ -55,7 +62,7 @@ describe('Functional Browser Tests using AFT-UI-BROWSERS', () => {
                 await loginPage.content().then(c => c.getLoginButton()).then(button => button.click());
                 await tw.logMgr.info('no exception thrown on click');
             }).withLogManager(tw.logMgr).and.withTestIds('C7890');
-        }).withDescription('can recover from StaleElementExceptions automatically');
+        }).withLogManager(aft.logMgr);
     });
 
     const uiplatforms = [
@@ -65,7 +72,8 @@ describe('Functional Browser Tests using AFT-UI-BROWSERS', () => {
     ];
     for (var i=0; i<uiplatforms.length; i++) {
         let uiplatform = uiplatforms[i];
-        it(`can run with multiple uiplatforms: ${uiplatform}`, async () => {
+        it(`can run with multiple uiplatforms: ${uiplatform}`, async function() {
+            const aft = new AftLog(this);
             await verifyWithBrowser(async (tw: BrowserVerifier) => {
                 let loginPage: HerokuLoginPage = await tw.session.getFacet<HerokuLoginPage, BrowserFacetOptions>(HerokuLoginPage);
                 await loginPage.navigateTo();
@@ -76,7 +84,7 @@ describe('Functional Browser Tests using AFT-UI-BROWSERS', () => {
                     .withBackOff('exponential')
                     .withMaxDuration(20000);
                 return await loginPage.getMessage();
-            }).withDescription(`can run with multiple uiplatforms: ${uiplatform}`)
+            }).withLogManager(aft.logMgr)
             .and.withBrowserSessionOptions({uiplatform: uiplatform})
             .returns(containing("You logged into a secure area!"));
         });
